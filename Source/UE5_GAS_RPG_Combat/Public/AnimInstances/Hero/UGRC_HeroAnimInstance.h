2af42dc55b84868eb2ffd2dfbf884f1f4686674e
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-03-23 22:44:27 +0530

    More Anim States added for Player

--- Source/UE5_GAS_RPG_Combat/Public/AnimInstances/Hero/UGRC_HeroAnimInstance.h
+++ Source/UE5_GAS_RPG_Combat/Public/AnimInstances/Hero/UGRC_HeroAnimInstance.h
@@ -4,8 +4,28 @@
 #include "AnimInstances/UGRC_CharacterAnimInstance.h"
 #include "UGRC_HeroAnimInstance.generated.h"
 
+class AUGRC_HeroCharacter;
+
 UCLASS()
 class UE5_GAS_RPG_COMBAT_API UUGRC_HeroAnimInstance : public UUGRC_CharacterAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	// ~ Begin UAnimInstance interface
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+	// ~ End UAnimInstance interface
+	
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+	TObjectPtr<AUGRC_HeroCharacter> OwningHeroCharacter;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	float IdleElapsedTime;
 };
