8868e35ac663e9427c201e3d4d2e26ed61d0d555
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-03-24 23:07:16 +0530

    Hero Ability Set Created

--- Source/UE5_GAS_RPG_Combat/Private/DataAssets/StartupData/UGRC_DataAsset_HeroStartupData.cpp
+++ Source/UE5_GAS_RPG_Combat/Private/DataAssets/StartupData/UGRC_DataAsset_HeroStartupData.cpp
@@ -1,1 +1,26 @@
 #include "DataAssets/StartupData/UGRC_DataAsset_HeroStartupData.h"
+#include "AbilitySystem/Abilities/UGRC_GameplayAbility.h"
+#include "AbilitySystem/UGRC_AbilitySystemComponent.h"
+
+bool FUGRC_HeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UUGRC_DataAsset_HeroStartupData::GiveToAbilitySystemComponent(TObjectPtr<UUGRC_AbilitySystemComponent> InASCToGive,
+	int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+	
+	for (const FUGRC_HeroAbilitySet& AbilitySet : HeroStartupAbilitySets)
+	{
+		if (!AbilitySet.IsValid()) continue;
+		
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.GetDynamicSpecSourceTags().AddTag(AbilitySet.InputTag);
+		
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}
--- Source/UE5_GAS_RPG_Combat/Public/DataAssets/StartupData/UGRC_DataAsset_HeroStartupData.h
+++ Source/UE5_GAS_RPG_Combat/Public/DataAssets/StartupData/UGRC_DataAsset_HeroStartupData.h
@@ -2,10 +2,32 @@
 
 #include "CoreMinimal.h"
 #include "DataAssets/StartupData/UGRC_DataAsset_StartupDataBase.h"
+#include "GameplayTagContainer.h"
 #include "UGRC_DataAsset_HeroStartupData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FUGRC_HeroAbilitySet
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UUGRC_GameplayAbility> AbilityToGrant;
+	
+	bool IsValid() const;
+};
 
 UCLASS()
 class UE5_GAS_RPG_COMBAT_API UUGRC_DataAsset_HeroStartupData : public UUGRC_DataAsset_StartupDataBase
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void GiveToAbilitySystemComponent(TObjectPtr<UUGRC_AbilitySystemComponent> InASCToGive, int32 ApplyLevel = 1) override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "StartupData", meta = (TitleProperty = "InputTag"))
+	TArray<FUGRC_HeroAbilitySet> HeroStartupAbilitySets;
 };
