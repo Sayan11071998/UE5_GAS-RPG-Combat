12b7e134fa1ab905e8e120a48dd8052b2b369a11
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-03-25 00:21:56 +0530

    Player Master Anim Layer Created

--- Source/UE5_GAS_RPG_Combat/Private/AnimInstances/Hero/UGRC_HeroLinkedAnimLayer.cpp
+++ Source/UE5_GAS_RPG_Combat/Private/AnimInstances/Hero/UGRC_HeroLinkedAnimLayer.cpp
@@ -1,1 +1,7 @@
 #include "AnimInstances/Hero/UGRC_HeroLinkedAnimLayer.h"
+#include "AnimInstances/UGRC_CharacterAnimInstance.h"
+
+UUGRC_CharacterAnimInstance* UUGRC_HeroLinkedAnimLayer::GetHeroAnimInstance() const
+{
+	return Cast<UUGRC_CharacterAnimInstance>(GetOwningComponent()->GetAnimInstance());
+}
--- Source/UE5_GAS_RPG_Combat/Public/AnimInstances/Hero/UGRC_HeroLinkedAnimLayer.h
+++ Source/UE5_GAS_RPG_Combat/Public/AnimInstances/Hero/UGRC_HeroLinkedAnimLayer.h
@@ -4,8 +4,14 @@
 #include "AnimInstances/UGRC_BaseAnimInstance.h"
 #include "UGRC_HeroLinkedAnimLayer.generated.h"
 
+class UUGRC_CharacterAnimInstance;
+
 UCLASS()
 class UE5_GAS_RPG_COMBAT_API UUGRC_HeroLinkedAnimLayer : public UUGRC_BaseAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintPure, meta = (BlueprintThreadSafe))
+	UUGRC_CharacterAnimInstance* GetHeroAnimInstance() const;
 };
