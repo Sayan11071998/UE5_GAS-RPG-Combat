29ca54592f41a4216836a7f59c7b9a79b9b937f9
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-03-26 23:55:42 +0530

    Basic Attribute Set Init

--- Source/UE5_GAS_RPG_Combat/Public/AbilitySystem/UGRC_AttributeSet.h
+++ Source/UE5_GAS_RPG_Combat/Public/AbilitySystem/UGRC_AttributeSet.h
@@ -2,10 +2,44 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/UGRC_AbilitySystemComponent.h"
 #include "UGRC_AttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 UCLASS()
 class UE5_GAS_RPG_COMBAT_API UUGRC_AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	UUGRC_AttributeSet();
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UUGRC_AttributeSet, CurrentHealth)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UUGRC_AttributeSet, MaxHealth)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UUGRC_AttributeSet, CurrentRage)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UUGRC_AttributeSet, MaxRage)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UUGRC_AttributeSet, AttackPower)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UUGRC_AttributeSet, DefensePower)
 };
