15e336708df27d695ed291beeff68460f713190c
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-03-30 01:16:47 +0530

    Basic AI Perception Created

--- Source/UE5_GAS_RPG_Combat/Private/Controllers/UGRC_AIController.cpp
+++ Source/UE5_GAS_RPG_Combat/Private/Controllers/UGRC_AIController.cpp
@@ -1,5 +1,7 @@
 #include "Controllers/UGRC_AIController.h"
 #include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 #include "UGRC_DebugHelper.h"
 
@@ -10,4 +12,21 @@ AUGRC_AIController::AUGRC_AIController(const FObjectInitializer& ObjectInitializ
 	{
 		Debug::Print(TEXT("UCrowdFollowingComponent valid"));
 	}
+	
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("EnemySenseConfig_Sight"));
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+	
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("EnemyPerceptionComponent"));
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &AUGRC_AIController::OnEnemyPerceptionUpdated);
+}
+
+void AUGRC_AIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
 }
--- Source/UE5_GAS_RPG_Combat/Public/Controllers/UGRC_AIController.h
+++ Source/UE5_GAS_RPG_Combat/Public/Controllers/UGRC_AIController.h
@@ -4,6 +4,10 @@
 #include "AIController.h"
 #include "UGRC_AIController.generated.h"
 
+struct FAIStimulus;
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+
 UCLASS()
 class UE5_GAS_RPG_COMBAT_API AUGRC_AIController : public AAIController
 {
@@ -11,4 +15,14 @@ class UE5_GAS_RPG_COMBAT_API AUGRC_AIController : public AAIController
 	
 public:
 	AUGRC_AIController(const FObjectInitializer& ObjectInitializer);
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UAIPerceptionComponent> EnemyPerceptionComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UAISenseConfig_Sight> AISenseConfig_Sight;
+	
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 };
