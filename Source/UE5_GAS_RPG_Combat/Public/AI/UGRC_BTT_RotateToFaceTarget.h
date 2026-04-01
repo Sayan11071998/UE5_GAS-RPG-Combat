9ec373d39bec934b5fddac836f19bfa41da94385
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-02 00:23:39 +0530

    Enemy Rotation To Attack Player Implemented

--- Source/UE5_GAS_RPG_Combat/Public/AI/UGRC_BTT_RotateToFaceTarget.h
+++ Source/UE5_GAS_RPG_Combat/Public/AI/UGRC_BTT_RotateToFaceTarget.h
@@ -34,6 +34,13 @@ class UE5_GAS_RPG_COMBAT_API UUGRC_BTT_RotateToFaceTarget : public UBTTaskNode
 	virtual FString GetStaticDescription() const override;
 	// ~ End UBTNode Interface
 	
+	// ~ Begin UBTTaskNode Interface
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	// ~ End UBTTaskNode Interface
+	
+	bool HasReachedAnglePrecision(TObjectPtr<APawn> QueryPawn, TObjectPtr<AActor> TargetActor) const;
+	
 	UPROPERTY(EditAnywhere, Category = "Face Target")
 	float AnglePrecision;
 	
