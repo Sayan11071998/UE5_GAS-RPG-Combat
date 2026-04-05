a31858627a107feb7c72cad36254399e1037200f
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-05 12:50:07 +0530

    Target Lock Camera Offset Applied

--- Source/UE5_GAS_RPG_Combat/Public/AbilitySystem/Abilities/UGRC_HeroAbility_TargetLock.h
+++ Source/UE5_GAS_RPG_Combat/Public/AbilitySystem/Abilities/UGRC_HeroAbility_TargetLock.h
@@ -63,6 +63,9 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "TargetLock")
 	TObjectPtr<UInputMappingContext> TargetLockMappingContext;
 	
+	UPROPERTY(EditDefaultsOnly, Category = "TargetLock")
+	float TargetLockCameraOffsetDistance = 20.f;
+	
 	UPROPERTY()
 	TArray<AActor*> AvailableActorsToLock;
 	
