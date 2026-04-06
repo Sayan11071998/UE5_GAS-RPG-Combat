39e440f2dc6013e10beec159d5f2753fe79cf222
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-06 11:12:05 +0530

    Boss Enemy Health Bar UI Removed after Death

--- Source/UE5_GAS_RPG_Combat/Public/Components/UI/UGRC_EnemyUIComponent.h
+++ Source/UE5_GAS_RPG_Combat/Public/Components/UI/UGRC_EnemyUIComponent.h
@@ -4,8 +4,20 @@
 #include "Components/UI/UGRC_PawnUIComponent.h"
 #include "UGRC_EnemyUIComponent.generated.h"
 
+class UUGRC_WidgetBase;
+
 UCLASS()
 class UE5_GAS_RPG_COMBAT_API UUGRC_EnemyUIComponent : public UUGRC_PawnUIComponent
 {
 	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UUGRC_WidgetBase* InWidgetToRegister);
+	
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
+	
+private:
+	TArray<TObjectPtr<UUGRC_WidgetBase>> EnemyDrawnWidgets;
 };
