8780b01cb0e81387e65b198ba74528c89a6f0252
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-11 20:50:30 +0530

    Working on Pickup Ability

--- Source/UE5_GAS_RPG_Combat/Private/Items/PickUps/UGRC_StoneBase.cpp
+++ Source/UE5_GAS_RPG_Combat/Private/Items/PickUps/UGRC_StoneBase.cpp
@@ -1,1 +1,13 @@
 #include "Items/PickUps/UGRC_StoneBase.h"
+#include "Characters/UGRC_HeroCharacter.h"
+#include "AbilitySystem/UGRC_AbilitySystemComponent.h"
+#include "UGRC_GameplayTags.h"
+
+void AUGRC_StoneBase::OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (AUGRC_HeroCharacter* OverlappedHeroCharacter = Cast<AUGRC_HeroCharacter>(OtherActor))
+	{
+		OverlappedHeroCharacter->GetUGRC_AbilitySystemComponent()->TryActivateAbilityTag(UGRC_GameplayTags::Player_Ability_PickUp_Stones);
+	}
+}
--- Source/UE5_GAS_RPG_Combat/Public/Items/PickUps/UGRC_StoneBase.h
+++ Source/UE5_GAS_RPG_Combat/Public/Items/PickUps/UGRC_StoneBase.h
@@ -8,4 +8,14 @@ UCLASS()
 class UE5_GAS_RPG_COMBAT_API AUGRC_StoneBase : public AUGRC_PickUpBase
 {
 	GENERATED_BODY()
+	
+protected:
+	virtual void OnPickUpCollisionSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult	
+	) override;
 };
